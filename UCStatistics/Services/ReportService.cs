07ed92f7fc94085cd833827d30c439f4485a1777
Steviamp <74257777+Steviamp@users.noreply.github.com>
2025-07-09 10:33:05 +0300

    Add files via upload

---
+++ UCStatistics/Services/ReportService.cs
@@ -0,0 +1,32 @@
+﻿using UCStatistics.Repositories;
+using UCStatistics.Shared.DTOs;
+
+namespace UCStatistics.Services
+{
+    public class ReportService
+    {
+        private readonly IReportRepository _repository;
+
+        public ReportService(IReportRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<IEnumerable<OfficeInfo>> GetOfficesAsync()
+             => _repository.GetOfficesAsync();
+        public Task<IEnumerable<SummaryDto>> GetHistoricalAsync(FilterCriteria criteria)
+        {
+            return _repository.GetHistoricalAsync(criteria);
+        }
+
+        public Task<IEnumerable<ServiceSummaryDto>> GetServiceSummaryAsync(FilterCriteria criteria)
+        {
+            return _repository.GetServiceSummaryAsync(criteria);
+        }
+
+        public Task<IEnumerable<TicketDto>> GetTicketDetailsAsync(FilterCriteria criteria)
+        {
+            return _repository.GetTicketDetailsAsync(criteria);
+        }
+    }
+}
---
+++ UCStatistics/Shared/DTOs/FilterDialogModel.cs
@@ -0,0 +1,8 @@
+﻿namespace UCStatistics.Shared.DTOs
+{
+    public class FilterDialogModel
+    {
+        public List<OfficeInfo> Areas { get; set; } = new();
+        public FilterCriteria Criteria { get; set; } = new();
+    }
+}
