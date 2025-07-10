17fb595bafcd28dc3b001287cd686f93e5655ee8
Steviamp <74257777+Steviamp@users.noreply.github.com>
2025-07-10 12:32:01 +0300

    Delete UCStatistics directory

--- UCStatistics/Shared/DTOs/FilterDialogModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace UCStatistics.Shared.DTOs
-{
-    public class FilterDialogModel
-    {
-        public List<OfficeInfo> Areas { get; set; } = new();
-        public FilterCriteria Criteria { get; set; } = new();
-    }
-}
