4da98418ae53127d5fc2f31f3df145a460c149a4
Steviamp <74257777+Steviamp@users.noreply.github.com>
2025-07-16 11:16:10 +0300

    Add files via upload

---
+++ UCStatistics/wwwroot/app.js
@@ -0,0 +1,8 @@
+﻿window.downloadFile = (base64, filename) => {
+    const link = document.createElement('a');
+    link.download = filename;
+    link.href = 'data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,' + base64;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+};
