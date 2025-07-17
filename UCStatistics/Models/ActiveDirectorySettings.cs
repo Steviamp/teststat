7ae0511418bc44a8e440d80088a116cb9932c022
Steviamp <74257777+Steviamp@users.noreply.github.com>
2025-07-17 13:41:46 +0300

    Delete UCStatistics directory

--- UCStatistics/Models/ActiveDirectorySettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace UCStatistics.Models
-{
-    public class ActiveDirectorySettings
-    {
-        public string DepartmentPropertyName { get; set; } = "department";
-        public string RequiredGroupToAuthenticate { get; set; } = "Domain Users";
-    }
-}
